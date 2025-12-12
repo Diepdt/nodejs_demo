c36c277564b544709a6ec386bc39270239a55514
Diepdt <duongtuandiep2@gmail.com>
2025-12-13 05:35:52 +0700

    #35 static files

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,8 +1,8 @@
 import express, { Express } from 'express'; // const express = require('express');
 const router = express.Router();
 
-const webRoutes = (app: Express) => {
-    router.get("/", (req, res) => {
+const webRoutes = (app: Express) => { // khai bao 1 ham va dat ten: webRoutes
+    router.get("/", (req, res) => { // khai bao ham callback vo danh
         res.render("home.ejs");
     });
 
