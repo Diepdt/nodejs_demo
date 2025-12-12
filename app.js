bdf1c678a6bea49737108e4e86ceb66b7418a0c3
Diepdt <duongtuandiep2@gmail.com>
2025-12-12 08:12:06 +0700

    #24

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Hello world!")
+const express = require("express");
+
+const app = express();
+
+PORT = 8080;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/Diepdt", (req, res) => {
+    res.send("Hello Diepdt!");
+});
+
+app.listen(PORT, () => {
+    console.log(`My app is running on port ${PORT}`);
+});
+
